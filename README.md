2aaef5f2b7a72dd4a67d70db36b50d873f316e70
KenWhite02 <whiteken2002@gmail.com>
2021-02-08 21:27:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Project Title
+
+Netflix-Clone
+
+## Project Info
+
+This project was created using React. The aim of this project was to achieve a functional navigation bar and implement design just like the real stripe website which you can view at
+[https://stripe.com/](https://stripe.com/).
+
+## Screenshot
+
+![stipe](https://user-images.githubusercontent.com/68158625/107257490-c8452c00-6a4b-11eb-9c6c-b7e5096aa10e.png)
+
+## Technologies Used
+
+React Js & React Hooks.
+
+## View Project
+
+You can view the current project at [https://ken-stripe-menus.netlify.app/](https://ken-stripe-menus.netlify.app/)
+
+## Installation
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### License
+
+MIT © KenWhite.
---
+++ src/context.js
@@ -0,0 +1,49 @@
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu,
+        location,
+        page,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
